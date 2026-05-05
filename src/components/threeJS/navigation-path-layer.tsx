6aebac89464137d2e686a76538e7989db409fbd1
Smidtty <148750415+Smidtty@users.noreply.github.com>
2026-05-05 15:05:13 +0200

    navigation path animation

--- src/components/threeJS/navigation-path-layer.tsx
+++ src/components/threeJS/navigation-path-layer.tsx
@@ -4,7 +4,7 @@ import { useNavigation } from "#/lib/navigation-context"
 import { mapPointToThree } from "#/lib/three-utils"
 
 import { MARKER_LIFT } from "./constants"
-import { EdgePreview } from "./draw-primitives"
+import { AnimatedPathLine, EdgePreview } from "./draw-primitives"
 
 /**
  * Renders the navigation path as a continuous line connecting all nodes
@@ -16,20 +16,20 @@ export const NavigationPathLayer = () => {
   const { navigationPath } = useNavigation()
 
   const pathPoints = useMemo(() => {
-    if (!navigationPath || navigationPath.length < 2) {
-      return []
-    }
-
-    return navigationPath
-      .map((node) => {
-        return { x: node.x, y: node.y, floor: node.floor }
-      })
-      .map((point) => mapPointToThree(point, MARKER_LIFT))
+    if (!navigationPath || navigationPath.length < 2) return []
+    return navigationPath.map((node) =>
+      mapPointToThree({ x: node.x, y: node.y, floor: node.floor }, MARKER_LIFT)
+    )
   }, [navigationPath])
 
-  if (pathPoints.length < 2) {
-    return null
-  }
+  if (pathPoints.length < 2) return null
 
-  return <EdgePreview points={pathPoints} color="#4406e0" lineWidth={5} />
+  return (
+    <>
+      {/* Ghost trail underneath */}
+      <EdgePreview points={pathPoints} color="#4406e0" lineWidth={6} opacity={0.15} />
+      {/* Animated dash overlay */}
+      <AnimatedPathLine points={pathPoints} color="#4406e0" lineWidth={5} />
+    </>
+  )
 }
